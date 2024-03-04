704ee296c050175c9090747414b2c0567e3d0172
tomizdebski <tomizdebski@gmail.com>
2024-03-04 21:45:47 +0100

    add_config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,49 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        '3xl': '0 5px 100px 10px ',
+      },
+      colors: {
+        fiolet: "#645FC6",
+        blue: "#49C4E7",
+        green: "#67E3AF",
+        deep_gray: "#21212D",
+        dark_gray: "2C2C38",
+        gray: "656567",
+        light_gray: "E2E3E7",
+        light_blue: "E2F7FE",
+    
+      },
+      screens: {
+        'xs': '320px',
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+  
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+  
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+  
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+      },
+      spacing: {},
+      fontFamily: {
+        saira: "'Saira Condensed'",
+      
+      },
+    },
+    fontSize: {
+      large: "36px",
+      medium: "32px",
+      body_large: "16px",
+      body_medium: "14px",
+      inherit: "inherit",
     },
   },
   plugins: [],
