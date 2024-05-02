998456cab636927bc733840f29539b64f9709b91
tomizdebski <tomizdebski@gmail.com>
2024-05-02 20:30:36 +0200

    all-logic-done-save_local-storage-only

--- app/page.tsx
+++ app/page.tsx
@@ -3,57 +3,39 @@ import Board from "@/components/Board";
 import { BoardType } from "@/types";
 import { useState, useEffect } from "react";
 
-
-
 export default function Home() {
+  const [boards, setBoards] = useState<BoardType[]>(() => {
+    // Sprawdzanie czy dane istnieją w local storage, jeśli nie, zwraca domyślne dane
+    const storedBoards = localStorage.getItem("boards");
+    return storedBoards ? JSON.parse(storedBoards) : [{
+      id: "1",
+      title: "My Board",
+      columns: [
+        {
+          id: "1",
+          title: "To Do",
+        },
+        {
+          id: "2",
+          title: "In Progress",
+        },
+        {
+          id: "3",
+          title: "Done",
+        },
+      ],
+      tasks: [],
+    }];
+  });
+
+  const [activeBoard, setActiveBoard] = useState<BoardType>(boards[0]);
 
   useEffect(() => {
-    
-  }, []);
-  
-
-  const [boards, setBoards] = useState<BoardType[]>([{
-    id: "1",
-    title: "My Board",
-    columns: [
-      {
-        id: "1",
-        title: "To Do",
-      },
-      {
-        id: "2",
-        title: "In Progress",
-      },
-      {
-        id: "3",
-        title: "Done",
-      },
-    ],
-    tasks: [
-      {
-        id: "11",
-        columnId: "1",
-        content: "Task 1",
-      },
-      {
-        id: "22",
-        columnId: "1",
-        content: "Task 2",
-      },
-      {
-        id: "33",
-        columnId: "2",
-        content: "Task 3",
-      },
-      {
-        id: "44",
-        columnId: "3",
-        content: "Task 4",
-      },
-    ],
-  }]);
-  
-  const [activeBoard, setActiveBoard] = useState<BoardType>(boards[0]);
+    // Zapisywanie danych do local storage przy odmontowywaniu komponentu
+    return () => {
+      localStorage.setItem("boards", JSON.stringify(boards));
+    };
+  }, [boards]);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
